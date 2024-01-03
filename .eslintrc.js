20e49e615ed008800a3a0c60cc589a0d541cc117
tripplen63 <tripplen63@gmail.com>
2024-01-03 09:21:18 +0200

    exercise 11.6: Fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,49 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    "jest/globals": true,
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest/globals": true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    ecmaVersion: 2018,
-    sourceType: "module",
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
-  plugins: ["react", "jest"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    eqeqeq: "error",
+  "plugins": [
+    "react", "jest"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+        2
+      ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "eqeqeq": "error",
     "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": ["error", { allow: ["warn", "error"] }], // Allow console.warn and console.error
-    "react/prop-types": 0,
-    "no-undef": "off",
-  },
-};
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": "error",
+    "react/prop-types": 0
+  }
+}
