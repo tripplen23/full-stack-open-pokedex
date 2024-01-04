66303054b4c2dfc7e45518cd2dd225eaac6d2620
tripplen63 <tripplen63@gmail.com>
2024-01-04 15:17:22 +0200

    update 11.10-11.11: Deploy and automation to Render

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('new version deployed')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
---
+++ build_step.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Build script"
+
+# Add your build and start commands here
+npm install
+npm run build
+npm run start-prod
